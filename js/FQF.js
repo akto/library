19f14d84e5a1d627248da465a0d36de3877a6305
Egemen <yusege@gmail.com>
2019-01-11 10:47:47 +0300

    create FQF.js

---
+++ js/FQF.js
@@ -0,0 +1,38 @@
+/*
+# I'll write a script to translate qwerty keyboard keys to F keyboard for mostly Turkish Citizins.
+# The following comparisons will be changed to two array for quick replacements.
+
+("F", "Q");
+("G", "W");
+("Ğ", "E");
+("I", "R");
+("O", "T");
+("D", "Y");
+("R", "U");
+("N", "I");
+("H", "O");
+("P", "P");
+("Q", "Ğ");
+("W", "Ü");
+
+("U", "A");
+("İ", "S");
+("E", "D");
+("A", "F");
+("Ü", "G");
+("T", "H");
+("K", "J");
+("M", "K");
+("L", "L");
+("Y", "Ş");
+("Ş", "İ");
+
+("J", "Z");
+("Ö", "X");
+("V", "C");
+("C", "V");
+("Ç", "B");
+("Z", "N");
+("S", "M");
+("B", "Ö");
+*/
