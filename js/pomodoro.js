a5cec84e6313ad29a494fa50ad614d82d5939adb
Egemen <yusege@gmail.com>
2021-02-19 21:52:46 +0300

    Update pomodoro.js

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,6 +1,6 @@
 var Pomodoro = {
 	intervalID : 0,
-	countdown : 0,
+	tictoc : 0,
 	sessionTime : 20, //Default Session Time
 	breakTime : 5, //Default Break Time
 	status : "session", // @param session or break 
@@ -8,16 +8,20 @@ var Pomodoro = {
 	setSessionTime : function( sTime ){ this.sessionTime = sTime*60; },
 	setBreakTime : function( bTime ){ this.breakTime = bTime*60; },
 	getSessionTime : function () { return this.sessionTime; },
-	getBreakTime : function(){ return this.breakTime; }
+	getBreakTime : function(){ return this.breakTime; },
+	changeState : function(){
+		this.status == "session" ? this.status = "break" : this.status = "session";
+	},
 	start : function(){ 
 		this.stop();
+		this.tictoc = this.getSessionTime();
 		this.intervalID = setInterval( 
 			function(){
-			    console.log(Pomodoro.countdown);
-				if(Pomodoro.countdown == 0){
+			    console.log(Pomodoro.tictoc);
+				if(Pomodoro.tictoc == 0){
 				    Pomodoro.stop();
 				}
-				Pomodoro.countdown--;
+				Pomodoro.tictoc--;
 			} ,1000 );  
 	},
 	stop : function(){ clearInterval( this.intervalID ); }
