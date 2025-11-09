7ace0c3243e6eb972c7ab06f0a0725bbdf11fde9
Egemen <yusege@gmail.com>
2025-11-09 15:22:23 +0300

    Update project_gutenberg_readability_enhancement_hack.js

--- js/project_gutenberg_readability_enhancement_hack.js
+++ js/project_gutenberg_readability_enhancement_hack.js
@@ -12,7 +12,7 @@ Tempermonkey setup
 // @namespace    http://tampermonkey.net/
 // @version      2025-09-20
 // @description  try to take over the world!
-// @author       You
+// @author       Egemen AKTO
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
 // @match        https://www.gutenberg.org/*
